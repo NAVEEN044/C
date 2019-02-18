ac16d37364280d9daff27813510c99f40864012f
NAVEEN044 <47105845+NAVEEN044@users.noreply.github.com>
2019-02-18 16:25:37 +0530

    Update set1-3.c

--- set1-3.c
+++ set1-3.c
@@ -1,31 +1,15 @@
 #include <stdio.h>
-main()
-{    char ch;
-    printf("\nEnter Single Character: ");
-    scanf("%c", &ch);
-    switch (ch)
-    {
-    case 'a':
-    case 'A':
-        printf("\n\n%c is a vowel", ch);
-        break;
-    case 'e':
-    case 'E':
-        printf("\n\n%c is a vowel", ch);
-        break;
-    case 'i':
-    case 'I':
-        printf("\n\n%c is a vowel", ch);
-        break;
-    case 'o':
-    case 'O':
-        printf("\n\n%c is a vowel", ch);
-        break;
-    case 'u':
-    case 'U':
-        printf("\n\n%c is a vowel", ch);
-        break;
-    default:
-        printf("\n\n%c is not a vowel", ch);
-    }  getch();
+int main()
+{
+int ch;
+clrscr();
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("is a vowel");
+}
+else
+{
+printf("is a consonent");
+}  
 }
