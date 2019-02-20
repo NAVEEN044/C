724ce5a008e6c9df7810d220c49d152222a344d5
NAVEEN044 <47105845+NAVEEN044@users.noreply.github.com>
2019-02-20 16:32:05 +0530

    Create set2-4.c

---
+++ set2-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int start,end,i=0;
+printf("enter the  starting number");
+scanf("%d",&start);
+printf("enter the ending number");
+scanf("%d",&end);
+for(i=start;i<=end;i++)
+{
+if(i%2!=0)
+{
+printf("%d",i);
+}
+}
+return 0;
+
+}
