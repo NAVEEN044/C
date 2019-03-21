c1a2020a839ce9b655edc63f19fd7e86bdea5537
NAVEEN044 <47105845+NAVEEN044@users.noreply.github.com>
2019-03-21 15:48:57 +0530

    Create set35.c

---
+++ set35.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[20];
+int i,count=0;
+printf("enter the string");
+scanf("S",&str[i]);
+for(i=0;str[i]!=null;i++)
+{
+if(str[i]>=1||str[i]<=9)
+{
+count++;
+}
+printf("%d",count);
+}
